870963a3796bf5bfba15a8654a431810c9a2ce6e
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-08-01 13:13:15 +0300

    commit

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -9,18 +9,29 @@ use Illuminate\Support\Facades\Http;
 class QuestionController extends Controller
 {
     public function fetchInsert(){
-        $response = Http::get('https://quizapi.io/api/v1/questions -G',[
-            
-             'apiKey'  =>'2pi8MSMiCAywRm9xbabJS1uXWEv5Eh72cZ3ePQYx',
-              'limit'=> 10
-        ]);
-        return $response;
+        $response = Http::get('https://quizapi.io/api/v1/questions', [
+        'apiKey' => '2pi8MSMiCAywRm9xbabJS1uXWEv5Eh72cZ3ePQYx',
+        'limit' => 100
+    ]);
+        $questions = json_decode($response->body());
+        foreach ($questions as $q) {
+            $question = new Question();
+            $question->question = $q->question;
+            $question->answer_a = $q->answers->answer_a;
+            $question->answer_b = $q->answers->answer_b;
+            $question->answer_c = $q->answers->answer_c;
+            $question->save();
+        }
+
+        return "Data fetched from Quiz Api and inserted successfully!";
     }
 
-    public function show(){
 
+
+    public function show()
+    {
         $data['questions']= Question::all();
-        
+
         return view('welcome', $data);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuestionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Fetch
+Route::get('/fetch',[QuestionController::class, 'fetchInsert']);
+
+// Show
+Route::post('/fetch', [QuestionController::class, 'show']);
